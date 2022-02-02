fec789edf1ef3cb088528f62eb21caa4e9039787
= <=>
2022-02-02 20:59:03 +0100

    Enviando el archivo de prueba

--- glunits.go
+++ glunits.go
@@ -3,13 +3,19 @@ package main
 import (
     "fmt"
     "os"
+	"regexp"
 )
+
+var re = regexp.MustCompile("([0-9]+)([A-Za-z]+)")
 
 func main() {
 
 	//Command-line Arguments
 	args := os.Args[1:]
 	if len(args) < 4 {
-		fmt.Println(args)
+		matches := re.FindStringSubmatch(args[0])
+		fmt.Println(matches[1])
+		fmt.Println(matches[2])
+		//fmt.Println(args[0])
 	}
 }
