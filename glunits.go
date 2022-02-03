aa0fa465bc4f1ee3cdd39ed94345e8dee3887c59
= <=>
2022-02-03 11:52:38 +0100

    lowercase added

--- glunits.go
+++ glunits.go
@@ -4,14 +4,28 @@ import (
     "fmt"
     "os"
     "regexp"
+    "strings"
 )
 
-var re = regexp.MustCompile("([0-9.]+)([A-Za-z]+)")
+var re = regexp.MustCompile("([0-9.]+)([a-z/]+)")
+
+func lowercase(array []string) {
+	for i := 0; i < len(array); i++ {
+		array[i] = strings.ToLower(array[i])
+	}
+	return
+}
+
 
 func main() {
 
 	//Command-line Arguments
 	args := os.Args[1:]
+	
+	//Pass to lower case
+	lowercase(args)
+
+	//Check the number of arguments
 	if len(args) < 4 {
 		matches := re.FindStringSubmatch(args[0])
 		fmt.Println(matches[1])
